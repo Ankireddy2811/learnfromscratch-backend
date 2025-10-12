dcee87629344c36d732fafc71a7f8b3f828a80fc
Ankireddy2811 <ankireddyputtam@gmail.com>
2025-10-12 23:05:17 +0530

    spring boot initial setup

---
+++ backend/src/main/java/com/learnfromscratch/backend/LearnFromScratchBackendApplication.java
@@ -0,0 +1,13 @@
+package com.learnfromscratch.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnFromScratchBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnFromScratchBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/learnfromscratch/backend/LearnFromScratchBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learnfromscratch.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnFromScratchBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
